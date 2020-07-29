37a99eac51cd2def9cc890c1a680ee6ce8e13ec6
Francisval Cavalcante <francisvalcs@gmail.com>
2020-07-28 23:57:38 -0300

    Projeto basico Reactjs e Firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      token: 'Carregando...',
+      nome: '',
+      idade: ''
+    };
+
+    let firebaseConfig = {
+      apiKey: "AIzaSyByPTQtZi_XyA_8hbeeLGp3vBftunLFpDo",
+      authDomain: "reactapp-f5547.firebaseapp.com",
+      databaseURL: "https://reactapp-f5547.firebaseio.com",
+      projectId: "reactapp-f5547",
+      storageBucket: "reactapp-f5547.appspot.com",
+      messagingSenderId: "875597826568",
+      appId: "1:875597826568:web:a5d648d51cc57c69e31fbd",
+      measurementId: "G-BZRPTR8FY4"
+    };
+    // Initialize Firebase
+    if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+    }
+    /*firebase.analytics();*/
+
+    firebase.database().ref('token').on('value', (snapshot) => {
+      let state = this.state;
+      state.token = snapshot.val();
+      this.setState(state);
+    })
+
+    firebase.database().ref('usuarios').child(1).on('value', (snapshot) =>{
+        let state = this.state;
+        state.nome = snapshot.val().nome;
+        state.idade = snapshot.val().idade;
+        this.setState(state);
+    });
+
+  }
+  render(){
+    const {token, nome, idade} = this.state;
+
+    return(
+      <div>
+          <h1>Token: {token}</h1>  
+          <h1>Nome: {nome}</h1>
+          <h1>Idade: {idade}</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
